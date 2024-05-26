acea7148def26d9da108414c0b603fcc1b5280ef
kshired <shiroed1211@gmail.com>
2024-05-26 16:46:17 +0900

    init : init projects

---
+++ src/main/kotlin/emotion/wave/muselyctale/MuselyctaleApplication.kt
@@ -0,0 +1,11 @@
+package emotion.wave.muselyctale
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MuselyctaleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MuselyctaleApplication>(*args)
+}
