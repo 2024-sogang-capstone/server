34b07ea548b6be9e19758b36d12185d56d370e1a
kshired <shiroed1211@gmail.com>
2024-05-27 01:13:01 +0900

    faet : add cors setting

--- src/main/kotlin/emotion/wave/muselyctale/common/config/WebConfig.kt
+++ src/main/kotlin/emotion/wave/muselyctale/common/config/WebConfig.kt
@@ -3,12 +3,21 @@ package emotion.wave.muselyctale.common.config
 import emotion.wave.muselyctale.common.auth.LoginUserArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
     private val loginUserArgumentResolver: LoginUserArgumentResolver,
 ) : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+    }
+
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(loginUserArgumentResolver)
     }
